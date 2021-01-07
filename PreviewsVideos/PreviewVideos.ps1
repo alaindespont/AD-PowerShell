e241ed40a4f210eb2f618504486910061b1887bc
Alain Philip Despont <70163173+alaindespont@users.noreply.github.com>
2021-01-07 07:23:32 +0100

    Update PreviewVideos.ps1

--- PreviewsVideos/PreviewVideos.ps1
+++ PreviewsVideos/PreviewVideos.ps1
@@ -1,11 +1,11 @@
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
-###### Créé par : Alain Philip Despont
-###### Dernière mise à jour : 21.11.2020
-###### But du script : Créer des vidéos raccourcies pour les réseaux sociaux, en découpant des vidéos existantes.
+###### CrÃ©Ã© par : Alain Philip Despont
+###### DerniÃ¨re mise Ã  jour : 21.11.2020
+###### But du script : CrÃ©er des vidÃ©os raccourcies pour les rÃ©seaux sociaux, en dÃ©coupant des vidÃ©os existantes.
 ###### Logiciels additionnels : ffmpeg
-###### Notes : Get-Location fait que le script ne peut pas être executé via ISE, il faut l'executer dans le dossier contenant les vidéos.
+###### Notes : Get-Location fait que le script ne peut pas Ãªtre executÃ© via ISE, il faut l'executer dans le dossier contenant les vidÃ©os.
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
@@ -13,22 +13,22 @@
 #Emplacement FFMPEG
 $ffmpeg = "E:\ffmpeg-win-2.2.2\ffmpeg.exe"
 
-# Récupère l'emplacement du script (mis dans le même dossier que les vidéos à couper)
+# RÃ©cupÃ¨re l'emplacement du script (mis dans le mÃªme dossier que les vidÃ©os Ã  couper)
 $dossier = Get-Location
 
-# Récupère toutes les vidéos qui serviront à faire des preview
+# RÃ©cupÃ¨re toutes les vidÃ©os qui serviront Ã  faire des preview
 $toutesvideos = @(Get-ChildItem -name *.mp4)
-$temps_debut_video = Read-Host -prompt 'Debut de la découpe au format => HH:MM:SS'
-$temps_a_couper = Read-Host -prompt 'Taille de la vidéo finale au format=> HH:MM:SS'
+$temps_debut_video = Read-Host -prompt 'Debut de la dÃ©coupe au format => HH:MM:SS'
+$temps_a_couper = Read-Host -prompt 'Taille de la vidÃ©o finale au format=> HH:MM:SS'
 
-# Pour chaque video (videoacut) présentes, créer une commande et l'executer.
+# Pour chaque video (videoacut) prÃ©sentes, crÃ©er une commande et l'executer.
 foreach ($videoAcut in $toutesvideos){
 
-#Créé la commande
+#CrÃ©Ã© la commande
 $commande = "$ffmpeg" + " -ss " + "$temps_debut_video" + " -t " + "$temps_a_couper" + " -i " + "$dossier" + "\" + "$videoAcut" + " -vcodec copy -acodec copy " + "$dossier" + "\PREVIEW_" + "$videoAcut"
 
 #Execute la commande, on pipe la commande sur cmd parce que sinon elle ne s'execute simplement pas
-$commande | cmd
+cmd.exe /c "$commande"
 }
 
 
