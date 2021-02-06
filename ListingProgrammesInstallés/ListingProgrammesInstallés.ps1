64cd632ca1ab336bb59b2c655c9a16cb66675397
Alain Philip Despont <70163173+alaindespont@users.noreply.github.com>
2021-02-06 19:37:34 +0100

    Update ListingProgrammesInstallés.ps1
    Le script ne trouvais que les applciations 32bit. Maintenant les applciations des deux architectures seront retrouvées.

--- ListingProgrammesInstallés/ListingProgrammesInstallés.ps1
+++ ListingProgrammesInstallés/ListingProgrammesInstallés.ps1
@@ -1,8 +1,8 @@
-﻿########################################################################################################################
+########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
 ###### Créé par : Alain Philip Despont
-###### Dernière mise à jour : 21.11.2020
+###### Dernière mise à jour : 06.02.2021
 ###### But du script : Produire des listes des programmes, apps et mise à jour installées sur le système en les séparant si c'est Microsoft ou pas.
 ###### Logiciels additionnels : 
 ###### Notes : -NotMatch ne produit pas les résultat voulus, d'ou l'utilisation de -notcontains
@@ -10,17 +10,29 @@
 ########################################################################################################################
 ########################################################################################################################
 
-$chemin_temporaire = "$PSScriptRoot" + "/ProgrammesInstallés_temp.csv"
+$chemin_temporaire32 = "$PSScriptRoot" + "/ProgrammesInstallés_temp32.csv"
+$chemin_temporaire64 = "$PSScriptRoot" + "/ProgrammesInstallés_temp64.csv"
+$chemin_temporaire_full = "$PSScriptRoot" + "/ProgrammesInstallés_tempfull.csv"
 $chemin_Windows = "$PSScriptRoot" + "/ProgrammesWindows.csv"
 $chemin_PasWindows = "$PSScriptRoot" + "/ProgrammesPasWindows.csv"
 
+# Programmes 32 bit
 # Exporte tous les programmes installés dans un fichier CSV
 Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*| 
 Select-Object -Property DisplayName, DisplayVersion, Publisher, @{Name="InstallDate"; Expression={([datetime]::ParseExact($_.InstallDate, 'yyyyMMdd', $null)).toshortdatestring()}} |
-Export-Csv -NoTypeInformation -Path $chemin_temporaire
+Export-Csv -NoTypeInformation -Path $chemin_temporaire32
 
-# Importe le fichier temporaire, supprime les vides du tableur, crée un fichier contenant tous les programmes installés qui ne sont pas de Microsoft
-Import-Csv -Path $chemin_temporaire|
+# Programmes 64bit
+# Exporte tous les programmes installés dans un fichier CSV
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*| 
+Select-Object -Property DisplayName, DisplayVersion, Publisher, @{Name="InstallDate"; Expression={([datetime]::ParseExact($_.InstallDate, 'yyyyMMdd', $null)).toshortdatestring()}} |
+Export-Csv -NoTypeInformation -Path $chemin_temporaire64
+
+# Assemble les deux csv
+@(Import-Csv -path $chemin_temporaire32) + @(Import-Csv -path $chemin_temporaire64) | Export-Csv $chemin_temporaire_full
+
+# Importe le fichier temporaire complet, supprime les vides du tableur, crée un fichier contenant tous les programmes installés qui ne sont pas de Microsoft
+Import-Csv -Path $chemin_temporaire_full|
 Where-Object {$_.PSObject.Properties.Value -ne ''}|
 Where-Object {$_.PSObject.Properties.Value -notcontains "Microsoft"}|
 Where-Object {$_.PSObject.Properties.Value -notcontains "Microsoft Corporation"}|
@@ -28,9 +40,11 @@ Where-Object {$_.PSObject.Properties.Value -notcontains "Microsoft Corporations"
 Export-Csv -NoTypeInformation -Path $chemin_PasWindows
 
 # Réimporte le fichier, mais cette fois garde que ce qui est Microsoft
-Import-Csv -Path $chemin_temporaire|
+Import-Csv -Path $chemin_temporaire_full|
 Where-Object {$_.PSObject.Properties.Value -Match "Microsoft"}|
 Export-Csv -NoTypeInformation -Path $chemin_Windows
 
-#Nettoyer fichiers temporaires
-Remove-Item -Path $chemin_temporaire
+#Nettoye les fichiers temporaires
+Remove-Item -Path $chemin_temporaire32
+Remove-Item -Path $chemin_temporaire64
+Remove-Item -Path $chemin_temporaire_full
